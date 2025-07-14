bcf0dd32f912bf1cf9910b401264b00626efa200
faridepc78 <faridnewepc78@gmail.com>
2025-07-14 20:16:44 +0330

    update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,34 +11,31 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $table = 'users';
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $guarded =
+        [
+            'id',
+            'created_at',
+            'updated_at',
+        ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected $fillable =
+        [
+            'name',
+            'email',
+            'email_verified_at',
+            'password',
+        ];
+
+    protected $hidden =
+        [
+            'password',
+        ];
+
+    protected $casts =
+        [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
 }

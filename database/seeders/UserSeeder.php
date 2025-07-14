b769de57420bd2f3eaa2c9b498a7f485b363e353
faridepc78 <faridnewepc78@gmail.com>
2025-07-14 20:22:34 +0330

    add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        if (!User::query()
+            ->count()) {
+            User::factory(10)
+                ->create();
+        } else {
+            $this->command->warn('Users has already been created');
+        }
+    }
+}
